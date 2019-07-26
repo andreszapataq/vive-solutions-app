17b27677417101ec0b92def0f288f4ce484ceffb
Gustavo Zapata <tavordie@hotmail.com>
2019-07-26 16:54:46 +0100

    fix 'pdo undefined'

--- db/connect.php
+++ db/connect.php
@@ -1,18 +1,43 @@
 <?php
-    $host = 'localhost';
-    $user = 'tecnologico';
-    $password = 'CoolerMaster22!';
-    $dbname = 'tecnologico_vivesolutionsapp';
+    // $host = 'localhost';
+    // $user = 'tecnologico';
+    // $password = 'CoolerMaster22!';
+    // $dbname = 'tecnologico_vivesolutionsapp';
 
-    $dsn = 'mysql:host='. $host .';dbname='. $dbname;
+    // $dsn = 'mysql:host='. $host .';dbname='. $dbname;
 
-    $pdo = new PDO($dsn, $user, $password);
-    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+    // $pdo = new PDO($dsn, $user, $password);
+    // $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+
+    //tavo
+    function getConnection(){
+        $host = 'localhost';
+        $dbname = "fundyetb_muchat";
+        $user = "root";
+        $password = "";
+        $dsn = 'mysql:host='. $host .';dbname='. $dbname;
+
+        $pdo = new PDO($dsn, $user, $password);
+        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+        return $pdo;
+    }
+    //tavo
 
     function checkLogin($usuario, $contrasena){
-        $sql = 'SELECT * FROM Usuario WHERE usuario = :usuario AND contrasena = :contrasena';
+        // $sql = 'SELECT * FROM Usuario WHERE usuario = :usuario AND contrasena = :contrasena';
+
+        //tavo
+        $pdo = getConnection();
+        $sql = "SELECT * FROM users WHERE username = :username AND password = :password";
+        //tavo
+
         $stmt = $pdo->prepare($sql);
-        $stmt->execute([':usuario' => $usuario, ':contrasena' => $contrasena]);
+
+        //tavo
+        $stmt->execute(['username' => $usuario, 'password' => $contrasena]);
+        //tavo
+
+        // $stmt->execute(['usuario' => $usuario, 'contrasena' => $contrasena]);
         if($stmt->rowCount() > 0){
             $conectado = $stmt->fetchAll();
             return true;
