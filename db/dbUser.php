6c7c9981222eb672e7d11ce1b5eb36a530afbb2e
skullordie <andreszapataq@gmail.com>
2019-08-14 00:22:01 -0500

    dbUser

---
+++ db/dbUser.php
@@ -0,0 +1,12 @@
+<?php
+    /* $host = 'localhost';
+    $user = 'tecnologico';
+    $password = 'HypeR1111!';
+    $dbname = 'tecnologico_vivesolutionsapp'; */
+
+    $host = 'localhost';
+    $user = 'root';
+    $password = 'Master2233*';
+    $dbname = 'tecnologico_vivesolutionsapp';
+
+    $dsn = 'mysql:host='. $host .';dbname='. $dbname;
